9996a4727626bba803dffc8b15cc74b09965c090
EC2 Default User <137010302+Mitsuki2002@users.noreply.github.com>
2023-09-14 13:00:45 +0000

    ユーザー機能を持った本の感想を投稿するためのサイト

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,57 +1,9 @@
 class BookCommentsController < ApplicationController
-  # def create
-  #   book = Book.find(params[:book_id])
-  #   comment = current_user.book_comments.new(book_comment_params)
-  #   comment.book_id = book.id
-  #   comment.save
-  #   redirect_to books_path
-  # end
-  # def destroy
-  #   BookComment.find(params[:id]).destroy
-  #   redirect_to books_path(params[:book_id])
-  # end
-  # private
-
-  # def book_comment_params
-  #   params.require(:book_comment).permit(:comment)
-    
-    
-    
-  # end
-  
-  
-  
-  # def create
-  #   post_image = PostImage.find(params[:post_image_id])
-  #   comment = current_user.post_comments.new(post_comment_params)
-  #   comment.post_image_id = post_image.id
-  #   comment.save
-  #   redirect_to post_image_path(post_image)
-  # end
-
-  # def destroy
-  #   PostComment.find(params[:id]).destroy
-  #   redirect_to post_image_path(params[:post_image_id])
-  # end
-
-  # private
-
-  # def post_comment_params
-  #   params.require(:post_comment).permit(:comment)
-  # end
-  
-  
-  
-  
-  
-  
-  
-  
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
+    comment.save!
     redirect_to request.referer
   end
 
